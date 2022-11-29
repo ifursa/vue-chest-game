7145ebe35c3c2808e11c0b808cbfa777a9fd28dd
ifursa <ivanfursa7@gmail.com>
2022-11-29 13:24:23 +0200

    fix 4

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath:'/vue-chest-game/dist/'
+  productionSourceMap: false,  
+  publicPath:'/vue-chest-game/'
 })
