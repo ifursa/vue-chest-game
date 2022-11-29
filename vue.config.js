77840b1dcaf12c1023b1ca245cfca665113cf6f9
ifursa <ivanfursa7@gmail.com>
2022-11-29 18:08:09 +0200

    sh 1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: ''
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
+}
